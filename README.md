b906f1cbf7e019614a56a37df3479b13f7e6e425
Faengleans <piyawad.n@ku.th>
2018-08-10 14:22:51 +0700

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Data Structure
+    by Piyawad Namtachan (5910406256)
+    This repository contains programs for data structures.
---
+++ src/main/java/csku/datastructure/ArrayCollection.java
@@ -0,0 +1,22 @@
+package csku.datastructure;
+public class ArrayCollection implements Collection {
+    public void add(Object element) {
+
+    }
+
+    public void remove(Object element) {
+
+    }
+
+    public boolean isEmpty() {
+        return false;
+    }
+
+    public boolean contains(Object element) {
+        return false;
+    }
+
+    public int size() {
+        return 0;
+    }
+}
